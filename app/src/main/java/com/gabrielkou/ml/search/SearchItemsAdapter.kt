33cf438c4adbb807730033494078e8b5c0a3582e
gabrielkou <gabriel.kb@gmail.com>
2019-02-28 11:32:23 -0300

    List layout fix

--- app/src/main/java/com/gabrielkou/ml/search/SearchItemsAdapter.kt
+++ app/src/main/java/com/gabrielkou/ml/search/SearchItemsAdapter.kt
@@ -4,7 +4,10 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.gabrielkou.ml.R
 import com.gabrielkou.ml.model.ItemViewModel
 import com.gabrielkou.presentation.model.ItemView
@@ -26,13 +29,21 @@ class SearchItemsAdapter @Inject constructor(): RecyclerView.Adapter<SearchItems
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.titleText.text = (items[position].title)
+        var item = items[position]
+        holder.titleText.text = item.title
+
+        Glide.with(holder.itemView.context)
+            .load(item.imageUrl)
+            .apply(RequestOptions.circleCropTransform())
+            .into(holder.thumbnail)
     }
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         var titleText:TextView
+        var thumbnail: ImageView
         init {
-            titleText = view.findViewById(R.id.text_title)
+            titleText = view.findViewById(R.id.item_title)
+            thumbnail = view.findViewById(R.id.item_thumbnail)
         }
 
     }
